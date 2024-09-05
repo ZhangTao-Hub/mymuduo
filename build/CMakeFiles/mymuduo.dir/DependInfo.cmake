896a66f6249515b2417c1842ced18cc2b2231a9a
tao <tao@test.com>
2024-09-05 10:53:11 +0800

    first commit

---
+++ build/CMakeFiles/mymuduo.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/Acceptor.cc" "CMakeFiles/mymuduo.dir/Acceptor.obj" "gcc" "CMakeFiles/mymuduo.dir/Acceptor.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/Buffer.cc" "CMakeFiles/mymuduo.dir/Buffer.obj" "gcc" "CMakeFiles/mymuduo.dir/Buffer.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/Channel.cc" "CMakeFiles/mymuduo.dir/Channel.obj" "gcc" "CMakeFiles/mymuduo.dir/Channel.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/CurrentThread.cc" "CMakeFiles/mymuduo.dir/CurrentThread.obj" "gcc" "CMakeFiles/mymuduo.dir/CurrentThread.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/DefaultPoller.cc" "CMakeFiles/mymuduo.dir/DefaultPoller.obj" "gcc" "CMakeFiles/mymuduo.dir/DefaultPoller.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/EPollPoller.cc" "CMakeFiles/mymuduo.dir/EPollPoller.obj" "gcc" "CMakeFiles/mymuduo.dir/EPollPoller.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/EventLoop.cc" "CMakeFiles/mymuduo.dir/EventLoop.obj" "gcc" "CMakeFiles/mymuduo.dir/EventLoop.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/EventLoopThread.cc" "CMakeFiles/mymuduo.dir/EventLoopThread.obj" "gcc" "CMakeFiles/mymuduo.dir/EventLoopThread.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/EventLoopThreadPool.cc" "CMakeFiles/mymuduo.dir/EventLoopThreadPool.obj" "gcc" "CMakeFiles/mymuduo.dir/EventLoopThreadPool.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/InetAddress.cc" "CMakeFiles/mymuduo.dir/InetAddress.obj" "gcc" "CMakeFiles/mymuduo.dir/InetAddress.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/Logger.cc" "CMakeFiles/mymuduo.dir/Logger.obj" "gcc" "CMakeFiles/mymuduo.dir/Logger.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/Poller.cc" "CMakeFiles/mymuduo.dir/Poller.obj" "gcc" "CMakeFiles/mymuduo.dir/Poller.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/Socket.cc" "CMakeFiles/mymuduo.dir/Socket.obj" "gcc" "CMakeFiles/mymuduo.dir/Socket.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/TcpConnection.cc" "CMakeFiles/mymuduo.dir/TcpConnection.obj" "gcc" "CMakeFiles/mymuduo.dir/TcpConnection.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/TcpServer.cc" "CMakeFiles/mymuduo.dir/TcpServer.obj" "gcc" "CMakeFiles/mymuduo.dir/TcpServer.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/Thread.cc" "CMakeFiles/mymuduo.dir/Thread.obj" "gcc" "CMakeFiles/mymuduo.dir/Thread.obj.d"
+  "D:/Desktop/muduo项目资料/muduo项目资料/mymuduo/mymuduo/Timestamp.cc" "CMakeFiles/mymuduo.dir/Timestamp.obj" "gcc" "CMakeFiles/mymuduo.dir/Timestamp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
